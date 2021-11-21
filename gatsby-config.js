af10d62204369de41ec676ca7171ca6fa3f169a3
rita ferreira <rita.c.ferreira@live.com>
2021-11-21 23:40:48 +0000

    added images char design

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,13 @@ module.exports = {
     "gatsby-plugin-typescript",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },    
+    
   ],
 };
